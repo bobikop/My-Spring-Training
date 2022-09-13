6a079d65d2a892574b798ae1679ac5806343439e
Boban Milosevic <104642358+bobikop@users.noreply.github.com>
2022-09-12 22:37:24 -0400

    Spring

---
+++ spring-04-Autowiring/src/main/java/com/cydeo/streotype_annotation/config/PcConfig.java
@@ -0,0 +1,14 @@
+package com.cydeo.streotype_annotation.config;
+
+import com.cydeo.casefactory.Case;
+import com.cydeo.casefactory.DellCase;
+import com.cydeo.casefactory.Dimensions;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.cydeo.streotype_annotation")
+public class PcConfig {
+
+}
