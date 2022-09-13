3872e0de35667f3a3f4abf9ef1e03c873e02e1a0
Boban Milosevic <104642358+bobikop@users.noreply.github.com>
2022-09-12 22:33:23 -0400

    Spring  Autowiring

--- spring-04-Autowiring/src/main/java/com/cydeo/streotype_annotation/PC.java
+++ spring-04-Autowiring/src/main/java/com/cydeo/streotype_annotation/PC.java
@@ -4,19 +4,25 @@ import com.cydeo.streotype_annotation.casefactory.Case;
 import com.cydeo.streotype_annotation.monitorfactory.Monitor;
 import com.cydeo.streotype_annotation.motherboardfactory.Motherboard;
 import lombok.Getter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 @Getter
+@Component
 public class PC {
 
+    @Autowired
     private Case theCase;
+    @Autowired
     private Monitor monitor;
+    @Autowired
     private Motherboard motherboard;
 
-    public PC(Case theCase, Monitor monitor, Motherboard motherboard) {
-        this.theCase = theCase;
-        this.monitor = monitor;
-        this.motherboard = motherboard;
-    }
+//    public PC(Case theCase, Monitor monitor, Motherboard motherboard) {
+//        this.theCase = theCase;
+//        this.monitor = monitor;
+//        this.motherboard = motherboard;
+//    }
 
     public void powerUp() {
         theCase.pressPowerButton();
