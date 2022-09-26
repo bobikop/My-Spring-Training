2039aed7a53c059e6737574042727b62ea96f5a5
Boban Milosevic <104642358+bobikop@users.noreply.github.com>
2022-09-25 20:10:36 -0400

    Spring MVC

--- spring-09.MVC-Model/src/main/java/com/cydeo/controller/CarController.java
+++ spring-09.MVC-Model/src/main/java/com/cydeo/controller/CarController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 @Controller
-public class CartController {
+public class CarController {
 
     //localhost:8080/info?make=Honda
     @RequestMapping("/info")
