80e469737a7e0571abb2eaa38b663e33b240296d
Boban Milosevic <104642358+bobikop@users.noreply.github.com>
2022-09-25 20:20:22 -0400

    Spring Thymeleaf

---
+++ spring-10-Thymeleaf/src/main/java/com/cydeo/controller/StudentController.java
@@ -0,0 +1,35 @@
+package com.cydeo.controller;
+
+import com.cydeo.bootstarp.DataGenerator;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/student")
+public class StudentController {
+
+    @RequestMapping("/register") //localhost:8080/student/register
+    public String register(Model model){
+
+        model.addAttribute("students", DataGenerator.createStudent());
+
+        return "student/register";
+    }
+
+//    @RequestMapping("/welcome") //localhost:8080/student/welcome/welcome?id=3
+//    public String drop(@RequestParam int id){
+//
+//        System.out.println(id);
+//        return "student/welcome";
+//
+//    }
+
+    @RequestMapping("/welcome") //localhost:8080/student/welcome/welcome?id=3
+    public String drop(@RequestParam String name){
+
+        System.out.println(name);
+        return "student/welcome";
+    }
+}
